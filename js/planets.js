f5a796d8c7aabd0fc8f9a78236ea05f89220d512
Bertrand <bertrand.tang.dev@gmail.com>
2025-02-12 16:03:21 +0100

    implements planet details

--- js/planets.js
+++ js/planets.js
@@ -1,5 +1,6 @@
 const planetsTableTbody = document.querySelector(".planets_table tbody");
 const resultsNumber = document.querySelector('.number_results')
+
 const url = "https://swapi.dev/api/planets/";
 let nextUrl = url;
 
@@ -17,10 +18,8 @@ async function getAllPlanets () {
         const otherDataPlanets = otherPagesJson.results;
         addNewTableCells(otherDataPlanets);
         nextUrl = otherPagesJson.next ? otherPagesJson.next : null;
-    }
-    
-};
-
+    }   
+}
 function addNewTableCells(dataArray) {
     for (let index = 0; index < dataArray.length; index++) {
         const newRow = document.createElement('tr');
@@ -33,5 +32,24 @@ function addNewTableCells(dataArray) {
         newRow.appendChild(newHeader);
         newRow.appendChild(newCell);
         planetsTableTbody.appendChild(newRow);
+
+        newRow.addEventListener("click", () => {
+            const selectMessage = document.querySelector(".select_message");
+            selectMessage.classList.add("notvisible");
+            const planetCardDetail = document.querySelector(".planet_detail_card");
+            planetCardDetail.classList.remove("notvisible");
+            const planetNamePlaceholder = document.querySelector(".planet_name");
+            planetNamePlaceholder.innerHTML = dataArray[index].name;
+            const populationPlaceholder = document.querySelector(".population_info");
+            populationPlaceholder.innerHTML = "Population : " + dataArray[index].population;
+            const diameterPlaceholder = document.querySelector(".diameter_info");
+            diameterPlaceholder.innerHTML = dataArray[index].diameter;
+            const climatePlaceholder = document.querySelector(".climate_info");
+            climatePlaceholder.innerHTML = dataArray[index].climate;
+            const gravityPlaceholder = document.querySelector(".gravity_info");
+            gravityPlaceholder.innerHTML = dataArray[index].gravity;
+            const terrainPlaceholder = document.querySelector(".terrain_info");
+            terrainPlaceholder.innerHTML = dataArray[index].terrain;
+        });
     }
 }
